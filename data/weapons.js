346f7f80881148bb78f25c32597db51741077332
fzboy2008-ui <fzboy2008@gmail.com>
2026-03-09 19:29:24 +0530

    Create weapons.js

---
+++ data/weapons.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+"Flame Sword":{price:5000000,power:60,element:"🔥"},
+"Ocean Trident":{price:4500000,power:55,element:"🌊"},
+"Thunder Blade":{price:4000000,power:50,element:"⚡"},
+"Wind Katana":{price:3500000,power:45,element:"🍃"},
+"Stone Hammer":{price:3000000,power:40,element:"🪨"}
+
+};
