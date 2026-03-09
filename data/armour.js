f173c459c0537d09e2c66e72896de69ed3450df2
fzboy2008-ui <fzboy2008@gmail.com>
2026-03-09 19:35:03 +0530

    Create armour.js

---
+++ data/armour.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+"Phoenix Armor":{price:5000000,def:60,element:"🔥"},
+"Tidal Armor":{price:4500000,def:55,element:"🌊"},
+"Storm Armor":{price:4000000,def:50,element:"⚡"},
+"Sky Armor":{price:3500000,def:45,element:"🍃"},
+"Earth Armor":{price:3000000,def:40,element:"🪨"}
+
+};
