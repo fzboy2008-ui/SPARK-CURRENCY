57cc74dc4b9f69d2a4b3b2bff5285b398688f822
fzboy2008-ui <fzboy2008@gmail.com>
2026-03-09 19:28:30 +0530

    Create dragons.js

---
+++ data/dragons.js
@@ -0,0 +1,63 @@
+module.exports = {
+
+Phoenix:{
+element:"🔥 Fire",
+price:10000000,
+abilities:[
+"Flame Punch",
+"Blazing Storm",
+"Inferno Rage",
+"Phoenix Rebirth",
+"Solar Explosion"
+]
+},
+
+Triton:{
+element:"🌊 Water",
+price:9000000,
+abilities:[
+"Water Slash",
+"Tsunami Crash",
+"Aqua Prison",
+"Ocean Fury",
+"Leviathan Call"
+]
+},
+
+Rex:{
+element:"⚡ Lightning",
+price:8000000,
+abilities:[
+"Spark Strike",
+"Thunder Crash",
+"Lightning Cage",
+"Storm Fury",
+"Thunder God"
+]
+},
+
+Zephyr:{
+element:"🍃 Wind",
+price:7000000,
+abilities:[
+"Wind Blade",
+"Cyclone Spin",
+"Sky Dash",
+"Hurricane Rage",
+"Storm Breaker"
+]
+},
+
+Grog:{
+element:"🪨 Earth",
+price:6000000,
+abilities:[
+"Stone Punch",
+"Rock Smash",
+"Earthquake",
+"Mountain Rage",
+"Gaia Wrath"
+]
+}
+
+};
